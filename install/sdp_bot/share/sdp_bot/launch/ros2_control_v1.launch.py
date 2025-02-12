bf6aaa7bc2f2c7f9805c1d7a01edeff6cafdbd95
leoherms <hermosilloleoncio@gmail.com>
2025-02-12 00:28:08 -0800

    fixed string error

--- install/sdp_bot/share/sdp_bot/launch/ros2_control_v1.launch.py
+++ install/sdp_bot/share/sdp_bot/launch/ros2_control_v1.launch.py
@@ -2,6 +2,7 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 import os
 
@@ -10,7 +11,11 @@ def generate_launch_description():
     pkg_share = FindPackageShare(package='sdp_bot').find('sdp_bot')
     default_model_path = os.path.join(pkg_share, 'urdf', 'all_bot.urdf')
     controller_param_path = os.path.join(pkg_share, 'config', 'controller.yaml')
-    print(controller_param_path)
+    
+    robot_description = ParameterValue(
+        Command(['xacro ', LaunchConfiguration('model')]),
+        value_type=str
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -18,19 +23,18 @@ def generate_launch_description():
             default_value=default_model_path,
             description='Absolute path to robot urdf file'
         ),
-        # Publish robot description first
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             parameters=[{
-                'robot_description': Command(['xacro ', LaunchConfiguration('model')])
+                'robot_description': robot_description
             }],
         ),
         Node(
             package='controller_manager',
             executable='ros2_control_node',
             parameters=[
-                {'robot_description': Command(['xacro ', LaunchConfiguration('model')])},
+                {'robot_description': robot_description},
                 controller_param_path
             ],
         ),
